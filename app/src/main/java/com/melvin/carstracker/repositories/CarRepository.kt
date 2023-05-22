dab76e67d02a58ce59e500ed976f88a6475b65fa
MelvinAguilar <00067621@uca.edu.sv>
2023-05-22 13:40:26 -0600

    add repository layer

---
+++ app/src/main/java/com/melvin/carstracker/repositories/CarRepository.kt
@@ -0,0 +1,11 @@
+package com.melvin.carstracker.repositories
+
+import com.melvin.carstracker.data.carmodel.CarModel
+
+class CarRepository(private val cars: MutableList<CarModel>) {
+    //Get all Cars
+    fun getCars() = cars
+
+    //Add a car to the mutable list
+    fun addCard(_card: CarModel) = cars.add(_card)
+}
